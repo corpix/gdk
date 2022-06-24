d603d9a49430d8528fdd119df8edac481a67d14d
Dmitry Moskowski <me@corpix.ru>
2022-06-24 15:46:54 +0000

    add example app

---
+++ example/gdk-example-app/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "github.com/corpix/gdk/cli"
+  "github.com/corpix/gdk/config"
+  "github.com/corpix/gdk/log"
+)
+
+type Config struct {
+  *config.BaseConfig
+}
+
+func main() {
+  cli.New(
+    cli.WithName("gdk-example-app"),
+    cli.WithUsage("GDK example application"),
+    cli.WithDescription("Example application showing features of GDK"),
+    cli.WithConfigTools(
+      &Config{BaseConfig: &config.BaseConfig{}},
+      config.YamlUnmarshaler,
+      config.YamlMarshaler,
+    ),
+    cli.WithLogTools(),
+    cli.WithAction(func(ctx *cli.Context) error {
+      log.Info().Str("msg", "hello").Msg("greeting")
+      return nil
+    }),
+  ).RunAndExitOnError()
+}
