9943f16e6f4f4bb1438c7b34b2a7f51f5dc81b69
Dmitry Moskowski <me@corpix.ru>
2022-07-20 13:05:32 +0000

    fix http example

--- example/gdk-example-http/main.go
+++ example/gdk-example-http/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/corpix/gdk/cli"
 	"github.com/corpix/gdk/config"
+	"github.com/corpix/gdk/di"
 	"github.com/corpix/gdk/http"
 	"github.com/corpix/gdk/log"
 )
@@ -45,22 +46,27 @@ func main() {
 			config.YamlMarshaler,
 		),
 		cli.WithLogTools(conf.LogConfig),
-		cli.WithHttpTools(conf.HttpConfig, http.NewRouter(
-			func(r *http.Router) {
-				r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-					t := http.RequestSessionGet(conf.Http.Session, r)
-					greet, ok := t.Get("greet")
-					if !ok {
-						greet = time.Now().String()
-						t.Set("greet", greet)
-					}
-					w.Write([]byte(greet.(string)))
-				})
-				r.HandleFunc("/panic", func(w http.ResponseWriter, r *http.Request) {
-					panic("hello panic")
-				})
-			},
-		)),
+		cli.WithHttpTools(conf.HttpConfig,
+			http.WithInvoke(
+				di.Default,
+				func(h *http.Http) {
+					h.Router.
+						HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+							t := http.RequestSessionGet(conf.Http.Session, r)
+							greet, ok := t.Get("greet")
+							if !ok {
+								greet = time.Now().String()
+								t.Set("greet", greet)
+							}
+							w.Write([]byte(greet.(string)))
+						})
+					h.Router.
+						HandleFunc("/panic", func(w http.ResponseWriter, r *http.Request) {
+							panic("hello panic")
+						})
+				},
+			),
+		),
 		cli.WithAction(func(ctx *cli.Context) error {
 			log.Info().Str("msg", "hello").Msg("greeting")
 			return nil
